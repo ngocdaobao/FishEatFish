78e69a937d5be075580dfecd22d651b26de671ea
hauxle <165156584+hauxle@users.noreply.github.com>
2024-04-21 22:06:14 +0700

    Update PowerUp.java

--- src/fisheater/PowerUp.java
+++ src/fisheater/PowerUp.java
@@ -7,47 +7,47 @@ import java.awt.Color;
 
 public class PowerUp extends CoreObject
 {
-    private long duration;
-    
-    private String name;
-    private String icon = "fisheater/resources/fish/algae.png";
-    
-    private int counter = 0;
-    
-    private boolean isAlive = false;
-    
-    public PowerUp(int x, int y, int speed)
+    private long duration; //luu tham so cho thoi gian của PowerUp
+
+    private String name; //luu ten cua powerup
+    private String icon = "fisheater/resources/fish/algae.png"; //path dan den file luu icon cho object powerup
+
+    private int counter = 0; //theo doi thoi gian con lai cua powerup
+
+    private boolean isAlive = false; // kiem tra xem powerup con hieu luc hay khong
+
+    public PowerUp(int x, int y, int speed) //constructor của PowerUp, nhận x, y là tọa độ rơi, speed là vận tốc rơi
     {
         super(x,y);
-        
+
         this.genImage(icon);
-        
-        this.dx = 0;
+
+        this.dx = 0; // mặc định vận tốc theo phương x = 0 để PowerUp rơi thẳng đứng
         this.dy = speed;
         this.name = "";
         this.counter = 0;
-        this.isAlive = true;
-        
-        this.center = new Center();
-        
-        genPower();
+        this.isAlive = true; // mặc định PowerUp là active
+
+        this.center = new Center(); // gọi đến constructor của center trong coreobject
+
+        genPower(); // gọi phương thức genPower để khởi tạo, xem method này ở dưới
     }
-    
+
     public void move()
     {
         super.move();
-        
-        if(this.y > 600)
+
+        if(this.y > 600) //nếu PowerUp chạy ra khỏi màn hình trò chơi
         {
-            this.isAlive = false;
+            this.isAlive = false; // không active, không hiển thị trên màn hình
         }
     }
-    
+
     public void genPower()
     {
         Random gen = new Random();
         int pType = gen.nextInt(10);
-        
+
         if(pType < 5)
         {
             name = "Speed Up";
@@ -59,32 +59,30 @@ public class PowerUp extends CoreObject
             counter = 63*14;
         }
     }
-    
-    //when the player touches the power up
-    //it's moved off screen and waits for duration
-    //of the power up to end before producing another
-    //power up
+
+    // khi người chơi chạm vào PowerUp thì nó bien mat khoi man hinh
+    //sau khoang thoi gian counter thì mới xuất hiện trở lại
     public void setY(int y)
     {
         this.y = y;
         this.dy = 0;
     }
-    
+
     public void setAlive(boolean b)
     {
         this.isAlive = b;
     }
-    
+
     public String getName()
     {
         return this.name;
     }
-    
+
     public boolean isAlive()
     {
         return this.isAlive;
     }
-    
+
     public int getDuration()
     {
         counter--;
