f27581afa21c04a14f486a2f6a5559ee6def66a3
hauxle <165156584+hauxle@users.noreply.github.com>
2024-04-28 22:01:33 +0700

    Update Fish.java

--- src/fisheater/Fish.java
+++ src/fisheater/Fish.java
@@ -1,25 +1,52 @@
 package fisheater;
 
-public class Fish extends CoreObject 
+import java.util.Random;
+
+public class Fish extends CoreObject
 {
-	private int points = 25;
-	private String icon = "fisheater/resources/fish/smallfish";
-	
-	public Fish(int x, int y, int speed, String direction)
-	{
-		super(x,y);
-		
+    private int points;
+    private String icon;
+    public Fish(int x, int y, int speed, String direction) {
+        super(x, y);
+        int random = new Random().nextInt(100);
+        int threshold = 15;
+        // extra small 15%
+        if (random < threshold) {
+            points = 1;
+            icon = "fisheater/resources/fish/mpurplefish";
+        } // small 25%
+        else if (threshold <= random && random < threshold + 25) {
+            points = 5;
+            icon = "fisheater/resources/fish/nemo";
+        } // medium 20%
+        else if (threshold + 25 <= random && random < threshold + 45) {
+            points = 10;
+            icon = "fisheater/resources/fish/brownfish";
+        } // large 20%
+        else if (threshold + 45 <= random && random < threshold + 65) {
+            points = 15;
+            icon = "fisheater/resources/fish/dory";
+        } // extra large 15%
+        else if (threshold + 65 <= random && random < threshold + 80) {
+            points = 25;
+            icon = "fisheater/resources/fish/catim";
+        } // rare fish 5%
+        else if (threshold + 80 <= random && random < threshold + 85) {
+            points = 50;
+            icon = "fisheater/resources/fish/canau";
+        }
+
         icon += direction + ".png";
-		this.genImage(icon);
-		
-                this.center = new Center();
-		this.dy = 0;
-		this.dx = -speed;
-	}
-	
-	public int getPoints()
-	{ return this.points; }
-        
+        this.genImage(icon);
+
+        this.center = new Center();
+        this.dy = 0;
+        this.dx = -speed;
+    }
+
+    public int getPoints()
+    { return this.points; }
+
     public int getSpeed()
     {
         return dx;
