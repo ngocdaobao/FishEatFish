5e25f446f49e9973b43002fcc60809f04e1ea318
Lương Thái Khang <luongkhang448@gmail.com>
2024-04-29 12:06:39 +0700

    update

---
+++ src/music/MusicThread.java
@@ -0,0 +1,30 @@
+package music;
+
+import java.util.ArrayList;
+
+import javax.sound.sampled.Clip;
+
+public class MusicThread {
+	private String file;
+	private double volume;
+	public ArrayList<Music> arr = new ArrayList<Music>();
+	private int i=0;
+	public MusicThread(String path, double v) {
+		file = path;
+		volume = v;
+		arr.add(new Music(file, volume));
+	}
+	public void play() {
+		boolean notPlay = true;
+    	for(int i=0; i<arr.size() && notPlay; i++) {
+    		if (!arr.get(i).isPlaying()) {
+    			arr.get(i).play();
+    			notPlay = false;
+    		}
+    	}
+    	if (notPlay) {
+			arr.add(new Music(file, volume));
+			arr.get(arr.size()-1).play();
+		}
+    }
+}
