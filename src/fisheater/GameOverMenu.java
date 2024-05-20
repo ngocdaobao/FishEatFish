fa571bc79be28828b24dd153498deafa85e616da
Papaya13-03 <Papaya1303@gmail.com>
2024-05-21 02:16:15 +0700

    Tao logic giua cac menu

--- src/fisheater/GameOverMenu.java
+++ src/fisheater/GameOverMenu.java
@@ -5,29 +5,22 @@ import javax.swing.*;
 import java.awt.Image;
 import java.awt.event.MouseEvent;
 
-public class GameOver extends JPanel {
+public class GameOverMenu extends JPanel {
 
     private String backgroundImageUrl = "fisheater/resources/menus/GameOver.png";
 
-    int[] replayBtn = { 432, 458, 506, 827 };
+    int[] replayBtn = { 620, 31, 692, 245 };
+    int[] menuBtn = { 620, 1026, 692, 1242 };
     JLabel scoreLabel;
 
-    public GameOver(int width, int height) {
+    public GameOverMenu(int width, int height) {
         setSize(width, height);
         setLocation(0, 0);
-        setScore(0);
     }
 
     public Image getBackgroundImage() {
         ImageIcon ii = new ImageIcon(getClass().getClassLoader().getResource(backgroundImageUrl));
         return ii.getImage();
-    }
-
-    public void setScore(int score) {
-        scoreLabel = new JLabel(String.valueOf(score));
-        scoreLabel.setSize(50, 100);
-        scoreLabel.setLocation(500, 100);
-        add(scoreLabel);
     }
 
     public boolean replayPressed(MouseEvent e) {
@@ -39,4 +32,15 @@ public class GameOver extends JPanel {
             return false;
         return true;
     }
+
+    public boolean menuPressed(MouseEvent e) {
+        double x = e.getX();
+        double y = e.getY();
+        if (x < menuBtn[1] || x > menuBtn[3])
+            return false;
+        if (y < menuBtn[0] || y > menuBtn[2])
+            return false;
+        return true;
+    }
+
 }
--- src/fisheater/Window.java
+++ src/fisheater/Window.java
@@ -3,31 +3,27 @@ package fisheater;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
-public class Window extends JFrame
-{
+public class Window extends JFrame {
     private final int WIDTH = 1280;
     private final int HEIGHT = 755;
-    public Window()
-    {
-        add( new Board( WIDTH, HEIGHT ));
+
+    public Window() {
+        add(new Board(WIDTH, HEIGHT));
         ImageIcon icon = new ImageIcon("fisheater/resources/menus/icon.png");
 
-        // Đặt biểu tượng cho cửa sổ
         setIconImage(icon.getImage());
 
-        setTitle( "Fish Eater" );
-        setDefaultCloseOperation(EXIT_ON_CLOSE); // đóng chương trình khi cửa sổ bị đóng
-        setSize( WIDTH, HEIGHT );
-        setLocationRelativeTo(null); // đặt cửa sổ tại trung tâm của màn hình
+        setTitle("Fish Eater");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(WIDTH, HEIGHT);
+        setLocationRelativeTo(null);
         setVisible(true);
-        setResizable(false); // cố định kích cỡ của cửa sổ
-        
+        setResizable(false);
+
     }
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         new Window();
     }
-
 
 }
